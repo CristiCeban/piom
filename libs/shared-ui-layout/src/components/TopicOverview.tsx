3790ed789782c9fb85ff7388991859cbe908c4fe
cristianceban <cebancristi4444@gmail.com>
2023-01-06 21:40:28 +0200

    test

--- libs/shared-ui-layout/src/components/TopicOverview.tsx
+++ libs/shared-ui-layout/src/components/TopicOverview.tsx
@@ -11,7 +11,7 @@ export function TopicOverview({ topic }: TopicOverviewProps) {
   if (!topic) {
     return null;
   }
-  const { name, description, deadline, id, student_id, teacher_id } = topic;
+  const { name, description, deadline } = topic;
   return (
     <Center>
       <Box p="2" py="8" w="90%" bgColor={'blue.100'} borderRadius="2xl">
